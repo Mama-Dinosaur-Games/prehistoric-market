fdfe01652608636c151ee689703ca62d1c8d7a56
KyleNicholls19 <kyle.nicholls190@gmail.com>
2026-02-20 16:17:43 -0700

    Update market.go

--- backend/internal/market/market.go
+++ backend/internal/market/market.go
@@ -2,6 +2,7 @@ package market
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -14,15 +15,43 @@ func (m *Market) getStockPrice(stockName string) float32 {
 	return m.priceIndex[stockName]
 }
 
+type Stock struct {
+	Name       string
+	Price      float64
+	Volatility float64
+}
+
 func runSimulation(days <-chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 	day := 1
 
+	// Initialize everything
+	stocks := []Stock{
+		{"MEAT", 100.0, 0.005},
+		{"FISH", 50.0, 0.01},
+		{"PLANT", 20.0, 0.002},
+	}
+
 	for range days {
 
 		fmt.Printf("Day: %d\n", day) // placeholder for main functionality
+		for i := range stocks {
+			stocks[i].UpdatePrice(day)
+			fmt.Printf("%s: %.2f\n", stocks[i].Name, stocks[i].Price)
+		}
+
+		// Do more updates daily here
+
 		day++
 	}
+}
+
+func (s *Stock) UpdatePrice(day int) {
+	increase := 0.005 // increase volatility by 0.5% per day
+	vol := s.Volatility + increase*float64(day)
+
+	epsilon := rand.NormFloat64() * vol
+	s.Price *= 1 + epsilon
 }
 
 func Run() {
