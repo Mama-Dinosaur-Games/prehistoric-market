311fe58c052b72065cd2eec45ec8ece6a5db44ab
KyleNicholls19 <kyle.nicholls190@gmail.com>
2026-02-20 17:47:37 -0700

    make portfolios

--- backend/internal/market/market.go
+++ backend/internal/market/market.go
@@ -19,7 +19,12 @@ type Stock struct {
 	Name       string
 	Price      float64
 	Volatility float64
-	Bitcoin_miner int 
+	Points     int
+}
+
+type Portfolio struct {
+	Player int
+	Shares map[string]int
 }
 
 func runSimulation(days <-chan bool, wg *sync.WaitGroup) {
@@ -28,12 +33,17 @@ func runSimulation(days <-chan bool, wg *sync.WaitGroup) {
 
 	// Initialize everything
 	stocks := []Stock{
-		{"MEAT", 100.0, 0.005,1},
-		{"FISH", 50.0, 0.01,1},
-		{"PLANT", 20.0, 0.002,1},
-		{"EGG", 0.00, 0.00,1},
-		{"INSECT", 0.00, 0.00,1},
-		}
+		{"MEAT", 100.0, 0.005, 1},
+		{"FISH", 50.0, 0.01, 1},
+		{"PLANT", 20.0, 0.002, 1},
+		{"EGG", 0.00, 0.00, 1},
+		{"INSECT", 0.00, 0.00, 1},
+	}
+
+	portfolios := []Portfolio{
+		{0, map[string]int{"MEAT": 0, "FISH": 0, "PLANT": 0, "EGG": 0, "INSECT": 0}},
+		{1, map[string]int{"MEAT": 0, "FISH": 0, "PLANT": 0, "EGG": 0, "INSECT": 0}},
+	}
 
 	for range days {
 
@@ -41,6 +51,13 @@ func runSimulation(days <-chan bool, wg *sync.WaitGroup) {
 		for i := range stocks {
 			stocks[i].UpdatePrice(day)
 			fmt.Printf("%s: %.2f\n", stocks[i].Name, stocks[i].Price)
+		}
+
+		for _, p := range portfolios {
+			fmt.Printf("Player %d:\n", p.Player)
+			for stock, qty := range p.Shares {
+				fmt.Printf("  %s: %d shares\n", stock, qty)
+			}
 		}
 
 		// Do more updates daily here
@@ -54,7 +71,13 @@ func (s *Stock) UpdatePrice(day int) {
 	vol := s.Volatility + increase*float64(day)
 
 	epsilon := rand.NormFloat64() * vol
-	s.Price *= 1 + epsilon
+
+	// Update price based off the points
+	pointFactor := 0.002
+	pointEffect := float64(s.Points) * pointFactor
+
+	s.Price *= 1 + epsilon + pointEffect
+
 }
 
 func Run() {
